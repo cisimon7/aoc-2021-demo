1a9399a45f39f09c3042497c424650eb2ff860f4
cisimon7 <simontaneous1@hotmail.com>
2021-12-01 17:22:11 +0300

    Day 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return input.map { it.toInt() }
+            .asSequence()
+            .windowed(2)
+            .sumOf { pair -> if (pair[1] > pair[0]) 1L else 0L }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.map { it.toInt() }
+            .asSequence()
+            .windowed(3)
+            .map { it.sum() }
+            .windowed(2)
+            .sumOf { pair -> if (pair[1] > pair[0]) 1L else 0L }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01P1")
 
     val input = readInput("Day01")
     println(part1(input))
