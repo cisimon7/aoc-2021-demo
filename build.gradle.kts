ace3c1aba4a86b8daeb974861be88da4aeb76d66
cisimon7 <simontaneous1@hotmail.com>
2021-12-02 13:57:10 +0300

    puzzle 2 of the day

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.6.0"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
 }
 
 tasks {
--- src/day2/day2.kt
+++ src/day2/day2.kt
@@ -1,12 +1,18 @@
 package day2
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
 import readInput
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
 @OptIn(ExperimentalTime::class)
 fun main() {
-    fun part1(input: List<String>): String {
+
+    val json = Json { prettyPrint = true }
+
+    fun part1(input: List<String>): Map<String, Int> {
         var horizontal = 0
         var depth = 0
 
@@ -22,22 +28,52 @@ fun main() {
                 }
             }
 
-        return "Horizontal: $horizontal; Depth: $depth; Product: ${horizontal * depth}"
+        return mapOf(
+            "Horizontal" to horizontal,
+            "Depth" to depth,
+            "Product" to horizontal * depth
+        )
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun part2(input: List<String>): Map<String, Int> {
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
+
+        input.asSequence()
+            .map { it.split(" ") }
+            .map { Command.parse(it[0], it[1]) }
+            .forEach { command ->
+                when (command) {
+                    is Command.Forward -> {
+                        horizontal += command.units
+                        depth += (aim * command.units)
+                    }
+                    is Command.Down -> { aim += command.units }
+                    is Command.Up -> { aim -= command.units }
+                    is Command.None -> {  }
+                }
+            }
+
+        return mapOf(
+            "Horizontal" to horizontal,
+            "Depth" to depth,
+            "Aim" to aim,
+            "Product" to horizontal * depth
+        )
     }
 
     val testInput = readInput("day2/day2_test.txt")
     println(part1(testInput))
-    part2(testInput)
+    println(part2(testInput))
 
     val input = readInput("day2/day2.txt")
-    val (result, time) = measureTimedValue { part1(input) }
-    println("{\n\tTime: $time\n\tResult: $result\n}")
+    val (result1, time1) = measureTimedValue { part1(input) }
+    println(json.encodeToString(mapOf("Time (ms)" to time1.inWholeMilliseconds.toInt()) + result1))
 
-    measureTimedValue { part2(input) }
+    // TODO("Why is this taking way less time")
+    val (result2, time2) = measureTimedValue { part2(input) }
+    println(json.encodeToString(mapOf("Time (ms)" to time2.inWholeMilliseconds.toInt()) + result2))
 }
 
 sealed interface Command {
