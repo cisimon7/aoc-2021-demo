1962dcb22da342e934cc0c679931c68b92807ff2
cisimon7 <simontaneous1@hotmail.com>
2021-12-03 15:16:48 +0300

    puzzle 2 of the day

--- src/day3/day3.kt
+++ src/day3/day3.kt
@@ -37,10 +37,38 @@ fun main() {
         )
     }
 
-    fun part2(input: List<String>): Int {
-        val oRating = 0
-        val cRating = 0
-        return input.size
+    fun part2(input: List<String>): Map<String, String> {
+        val spliced: List<List<Char>> = input.map { it.asSequence().toList() }
+
+        val oxygenSeq = buildSeq(spliced) { acc, entry ->
+            when {
+                acc.value > entry.value -> acc
+                acc.value < entry.value -> entry
+                else -> if (acc.key) acc else entry
+            }
+        }
+
+        val carbonSeq = buildSeq(spliced) { acc, entry ->
+            when {
+                acc.value > entry.value -> entry
+                acc.value < entry.value -> acc
+                else -> if (acc.key) entry else acc
+            }
+        }
+
+        val oRating = oxygenSeq.takeWhile { !it.second }.last().third.first()
+        val cRating = carbonSeq.takeWhile { !it.second }.last().third.first()
+
+        val oDec = oRating.joinToString("").toInt(2)
+        val cDec = cRating.joinToString("").toInt(2)
+
+        return mapOf(
+            "Oxygen Rating" to oRating.joinToString(""),
+            "Carbon Rating" to cRating.joinToString(""),
+            "Oxygen Rating (dec)" to oDec.toString(),
+            "Carbon Rating (dec)" to cDec.toString(),
+            "Product" to (oDec * cDec).toString()
+        )
     }
 
     val testInput = readInput("day3/day3_test.txt")
@@ -51,6 +79,27 @@ fun main() {
     val (result1, time1) = measureTimedValue { part1(input) }
     println(mapOf("Time (ms)" to time1.toString()) + result1)
 
-//    val (result2, time2) = measureTimedValue { part2(input) }
-//    println(json.encodeToString(mapOf("Time (ms)" to time2.inWholeMilliseconds.toInt()) + result2))
+    val (result2, time2) = measureTimedValue { part2(input) }
+    println(mapOf("Time (ms)" to time2.toString()) + result2)
+}
+
+fun buildSeq(list: List<List<Char>>, func: (Map.Entry<Boolean, Int>, Map.Entry<Boolean, Int>)->Map.Entry<Boolean, Int>): Sequence<Triple<Int, Boolean, List<List<Char>>>> {
+    val size = list.first().size
+
+    return generateSequence(Triple(0, false, list)) { (step, end, prevList) ->
+        val bits = List(size) { idx ->
+            prevList.map { it[idx] }
+        }[step % size]
+
+        val mode = bits
+            .groupingBy { bit -> bit == '1' }
+            .eachCount()
+            .entries
+            .reduce { acc, entry -> func(acc,entry) }
+            .let { if (it.key) '1' else '0' }
+
+        val next = prevList.zip(bits).filter { (_, snd) -> snd == mode }.map { it.first }
+
+        Triple(step+1, next.size==1 && prevList==next, next)
+    }
 }
