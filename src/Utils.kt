dc90a6316c270b7f967fe94f8c594913e0e95f1f
cisimon7 <simontaneous1@hotmail.com>
2021-12-18 23:39:32 +0300

    Puzzle 2 of day 6 Done

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,7 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.time.ExperimentalTime
 
 /**
  * Reads lines from the given input txt file.
@@ -11,3 +12,10 @@ fun readInput(path: String) = File("src", path).readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+@OptIn(ExperimentalTime::class)
+fun <T> runWithTime(block: () -> T) {
+    val (result1, time1) = kotlin.time.measureTimedValue { block() }
+    println(mapOf("Time (ms)" to time1.toString()) + mapOf("Result" to result1))
+}
--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -1,118 +1,55 @@
 package day6
 
 import readInput
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
+import runWithTime
+import java.math.BigInteger
 
-@OptIn(ExperimentalTime::class, kotlin.ExperimentalStdlibApi::class)
 fun main() {
     fun part1(input: List<String>): Int {
 
-        var fishes: List<Int> = parse(input)
+        var fish: List<Int> = parse(input)
 
-        repeat(80) { _ ->
-            val birthFishesCount = fishes.count { days -> days == 0 }
-            fishes = fishes.map { days -> if (days == 0) 6 else days - 1 }
-            fishes = fishes + List(birthFishesCount) { 8 }
+        repeat(80) {
+            val birthFishCount = fish.count { days -> days == 0 }
+            fish = fish.map { days -> if (days == 0) 6 else days - 1 }
+            fish = fish + List(birthFishCount) { 8 }
         }
 
-        return fishes.count()
+        return fish.count()
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): BigInteger {
 
-        val fish = parse(input)
-        val countDays = 150
-        val firstCycle = 8 + 1
-        val otherCycle = 6 + 1
-
-        fun growthModel(newGen: Long, oldGen: Long, countDays: Int): Long {
-
-            return when {
-                countDays <= otherCycle -> (newGen + oldGen)
-                else -> {
-                    (if (countDays <= firstCycle) newGen else growthModel(newGen, newGen, countDays - firstCycle)) +
-                            growthModel(oldGen, oldGen, countDays - otherCycle)
-                }
-            }
-        }
-
-        val beforeStartFish = fish.filter { day -> countDays > day }
-
-        return beforeStartFish.sumOf { day ->
-            if (countDays - day > firstCycle)
-                growthModel(1, 0, countDays - day)
-            else
-                1
-        } +
-                fish.sumOf { day ->
-                    if (countDays - day > otherCycle)
-                        growthModel(0, 1, countDays - day)
-                    else 1
-                }
-    }
-
-    fun part3(input: List<String>): Long {
-
-        val fish = parse(input)
+        val days2Birth = parse(input)
         val countDays = 256
-        val firstCycle = 8 + 1
-        val otherCycle = 6 + 1
 
-        val fishGrowth = DeepRecursiveFunction<Triple<Long, Long, Int>, Long> { (newGen, oldGen, countDays) ->
-            when {
-                countDays <= otherCycle -> (newGen + oldGen)
-                else -> {
-                    (if (countDays <= firstCycle) newGen else callRecursive(
-                        Triple(
-                            newGen,
-                            newGen,
-                            countDays - firstCycle
-                        )
-                    )) +
-                            callRecursive(Triple(oldGen, oldGen, countDays - otherCycle))
+        /* Map<RemainingDays, FishCount> */
+        var daysLeft2Count = (0..8).associateWith { day -> days2Birth.count { it == day }.toBigInteger() }
+        val fishCountFromDaysLeft = { daysLeft: Int -> daysLeft2Count.getValue(daysLeft) }
+
+        repeat(countDays) {
+            daysLeft2Count = daysLeft2Count.mapValues { (dayLeft, _) ->
+                val prevCount = fishCountFromDaysLeft((dayLeft + 1) % 9)
+                when {
+                    (dayLeft == 6) -> prevCount + fishCountFromDaysLeft(0)
+                    else -> prevCount
                 }
             }
         }
 
-        val beforeStartFish = fish.filter { day -> countDays > day }
-
-        return beforeStartFish.sumOf { day ->
-            if (countDays - day > firstCycle)
-                fishGrowth(Triple(1, 0, countDays - day))
-            else
-                1
-        } +
-                fish.sumOf { day ->
-                    if (countDays - day > otherCycle)
-                        fishGrowth(Triple(0, 1, countDays - day))
-                    else 1
-                }
+        return daysLeft2Count.values.reduce { acc, count -> acc + count }
     }
 
     val testInput = readInput("day6/day6_test.txt")
     println(part1(testInput))
-//    println(part2(testInput))
 
     val input = readInput("day6/day6.txt")
-    val (result1, time1) = measureTimedValue { part1(input) }
-    println(mapOf("Time (ms)" to time1.toString()) + mapOf("Count" to result1))
-
-//    val (result3, time3) = measureTimedValue { part3(input) }
-//    println(mapOf("Time (ms)" to time3.toString()) + mapOf("Count" to result3))
-
-    val (result2, time2) = measureTimedValue { part3(input) }
-    println(mapOf("Time (ms)" to time2.toString()) + mapOf("Count" to result2))
+    runWithTime { part1(input) }
+    runWithTime { part2(input) }
 }
 
 fun parse(input: List<String>): List<Int> {
     return input.map {
         it.split(",").filter { it.isNotBlank() }.map { it.toInt() }
     }.flatten()
-}
-
-sealed interface FishStage {
-    object Born : FishStage
-    object FirstBirth : FishStage
-    object NormalBirth : FishStage
 }
